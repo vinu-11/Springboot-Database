d6bbf3113f124f4beaf91ec3d2ec0ef129b4a660
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-09 17:02:43 +0530

    Create
    New file for understanding JPA Database
    Dependency Added : Spring Web, Spring Data JPA, Apache Derby Database

---
+++ src/main/java/com/example/springbootdatabase/SpringbootDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdatabase/SpringbootDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
